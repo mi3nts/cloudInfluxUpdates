d5fa2a6ab5f57d5d701bf6d960a6e2d8803ab38b
lakihaomal <lhw150030@utdallas.edu>
2024-10-03 13:23:44 -0500

    first update

---
+++ firmware/xu4Mqtt/mintsXU4/mintsDefinitions.py
@@ -0,0 +1,32 @@
+
+from getmac import get_mac_address
+import serial.tools.list_ports
+import yaml
+import pandas as pd
+
+dataFolder                = "/home/teamlary/mintsData/raw"
+metaFolder                = "/home/teamlary/mintsData/meta"
+
+  
+
+
+credentialsFile       = 'mintsXU4/credentials.yml'
+
+mqttBroker            = "mqtt.circ.utdallas.edu"
+mqttPort              =  8883  # Secure port
+
+credentials            = yaml.load(open(credentialsFile), Loader=yaml.Loader)
+
+nodeInfo               = pd.read_csv('https://raw.githubusercontent.com/mi3nts/AirQualityAnalysisWorkflows/main/influxdb/nodered-docker/id_lookup.csv')
+
+
+if __name__ == "__main__":
+
+    print("Data Folder Raw      : {0}".format(dataFolder))
+    print("Credentials File     : {0}".format(credentialsFile))
+
+
+    
+    #-------------------------------------------#
+    print("Node Info :")
+    print(nodeInfo)
